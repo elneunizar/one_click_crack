63839ec50d8a0e3a4eff87767c5e316eaba421c0
root <root@localhost.localdomain>
2017-01-23 19:55:50 +0700

    choose an monitor interface

--- one_click_wifi_cracker.sh
+++ one_click_wifi_cracker.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
-#read a list of interface 
+
+#declare an variabel for interface
 declare -a interfaces
+#declare an array variabel for monitor interface
+declare -a mon_ints
+#read a list of interface 
 #let i=0
 readarray -t interfaces < <(/usr/bin/airmon | grep "-" | cut -f 1) 
-#choose an interface
-echo ${interfaces[@]}
+
+#enable monitor interface
+#echo ${interfaces[@]}
 for i in ${interfaces[@]}; do
 	#echo $i
-	airmon-ng start $i >& /dev/null
-
-	#choose an monitor interface
-	declare -a mon_ints
-	readarray -t mon_ints < <(iwconfig | egrep "\wmon | Monitor" | cut -d " " -f1) >& /dev/null
-	echo ${mon_ints[@]}
-	#mon_int=$(iwconfig | egrep "\wmon | Monitor" | cut -d " " -f1) >& /dev/null
-	echo $mon_int
-	#For every process of aircrack
+	airmon-ng start $i &>/dev/null
 done
+
+#choose an monitor interface
+readarray -t mon_ints < <(iwconfig 2> /dev/null | egrep "\wmon | Monitor" | cut -d " " -f1)
+echo ${mon_ints[@]}
+
