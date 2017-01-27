dbb294d185b0f2ca783d9491f8f3aa7b67c24e26
root <root@localhost.localdomain>
2017-01-27 10:23:17 +0700

    revision

--- one_click_wifi_cracker.sh
+++ one_click_wifi_cracker.sh
@@ -9,6 +9,7 @@ declare -A intTxPower
 #read a list of interface 
 
 #Get the transmision power
+#and set the interface
 function setInt {
 	readarray -t monInt < <(iwconfig 2>&1 | egrep "[Mm]onit\w" | awk '{print $1}')
 	readarray -t intInit < <(/usr/bin/airmon | grep "-" | cut -f 1)
@@ -25,25 +26,29 @@ function setInt {
 		       fi	       
 	       fi
        done
-	echo ${intTxPower[wlan0]}
+	#echo ${intTxPower[wlan0]}
 }
+#Attack the wireless
+function attackIt {
+}
+
 #for i in ${interfaces[@]}; do
 #	echo ${intTxPower[i]}
 #done
 
 #enable monitor interface
 #echo ${interfaces[@]}
-for i in ${interfaces[@]}; do
+#for i in ${interfaces[@]}; do
 	#echo $i
-	airmon-ng start $i &>/dev/null
-done
+#	airmon-ng start $i &>/dev/null
+#done
 
 #choose an monitor interface
-readarray -t mon_ints < <(iwconfig 2> /dev/null | egrep "\wmon | Monitor" | cut -d " " -f1)
+#readarray -t mon_ints < <(iwconfig 2> /dev/null | egrep "\wmon | Monitor" | cut -d " " -f1)
 #echo ${mon_ints[@]}
-if [ ${#mon_ints[@]} > 0 ]; then
-	echo ${mon_ints[@]}
-fi
+#if [ ${#mon_ints[@]} > 0 ]; then
+#	echo ${mon_ints[@]}
+#fi
 ### note ###
 # belajar tentang wireless transmit power sebagai tolak ukur memilib interface #
 
