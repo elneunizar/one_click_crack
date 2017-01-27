1d79766e95f234e192787530725bc053e15629c9
devnull <devnull@localhost.localdomain>
2017-01-27 09:59:33 +0700

    make it become a function

--- one_click_wifi_cracker.sh
+++ one_click_wifi_cracker.sh
@@ -4,9 +4,32 @@
 declare -a interfaces
 #declare an array variabel for monitor interface
 declare -a mon_ints
+#declare an variabel of transmission power
+declare -A intTxPower
 #read a list of interface 
-#let i=0
-readarray -t interfaces < <(/usr/bin/airmon | grep "-" | cut -f 1) 
+
+#Get the transmision power
+function setInt {
+	readarray -t monInt < <(iwconfig 2>&1 | egrep "[Mm]onit\w" | awk '{print $1}')
+	readarray -t intInit < <(/usr/bin/airmon | grep "-" | cut -f 1)
+	for i in ${intInit[@]}; do
+		intTxPower[$i]=$(iwconfig $i | egrep -o "Tx-Power=[0-9]+ dBm" | egrep -o "[0-9]+")
+	done
+	let i=0
+	for j in ${intTxPower[@]}; do
+		if [ $j  > 60 ];
+			if [ "${intInt[$((i++))]}" == "${monInt[$((i++))]}" ]; then
+			       #i will done it with a function later
+		       elif [ "${intInt[$((i++))]}" != "${monInt[$((i++))]}" ]; then
+			       #i will done it with a function later
+		       fi	       
+	       fi
+       done
+	echo ${intTxPower[wlan0]}
+}
+#for i in ${interfaces[@]}; do
+#	echo ${intTxPower[i]}
+#done
 
 #enable monitor interface
 #echo ${interfaces[@]}
@@ -17,5 +40,10 @@ done
 
 #choose an monitor interface
 readarray -t mon_ints < <(iwconfig 2> /dev/null | egrep "\wmon | Monitor" | cut -d " " -f1)
-echo ${mon_ints[@]}
+#echo ${mon_ints[@]}
+if [ ${#mon_ints[@]} > 0 ]; then
+	echo ${mon_ints[@]}
+fi
+### note ###
+# belajar tentang wireless transmit power sebagai tolak ukur memilib interface #
 
